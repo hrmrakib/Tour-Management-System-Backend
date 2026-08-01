4ca32558a5ea2e67b232ddd45fc821346e7463a8
hrmrakib <hrmrakib@gmail.com>
2026-08-01 20:20:05 +0600

    Add pre-save hook to generate unique slug for division based on name

--- src/app/modules/division/division.model.ts
+++ src/app/modules/division/division.model.ts
@@ -22,4 +22,19 @@ const divisionSchema = new Schema(
   { timestamps: true },
 );
 
+divisionSchema.pre("save", async function (next) {
+  if (this.isModified("name")) {
+    const baseSlug = this.name.toLowerCase().split(" ").join("-");
+    let slug = `${baseSlug}-division`;
+
+    let counter = 0;
+    while (await Division.exists({ slug })) {
+      slug = `${slug}-${counter++}`; // dhaka-division-2
+    }
+
+    this.slug = slug;
+  }
+  // next();
+});
+
 export const Division = model<IDivision>("Division", divisionSchema);
